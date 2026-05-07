02062acd8c408ca21d904cf45db6855df8becd16
dheWeb <interns.dhe@gmail.com>
2026-05-07 15:16:46 +0530

    Update layout.tsx

--- dhe.org.in-main/src/app/layout.tsx
+++ dhe.org.in-main/src/app/layout.tsx
@@ -100,8 +100,10 @@ export default function RootLayout({
         <Modal isOpen={isModalOpen} onClose={closeModal}>
         <div className="text-white p-4 rounded-lg flex flex-col items-center justify-center text-center text-base md:text-xl font-semibold bg-primary">
             <p>
-     <strong>शिक्षा महाकुंभ अभियान</strong> – 6th Edition is going to be held
-              at <strong>NIT Hamirpur</strong>.            </p>
+  <strong>शिक्षा महाकुंभ अभियान</strong> – 6th Edition at{" "}
+  <strong>NIT Hamirpur</strong> from{" "}
+  <strong>9th Oct to 11th Oct 2026</strong>.
+</p>
             
           </div>
 
